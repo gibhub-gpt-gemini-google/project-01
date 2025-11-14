07c198213737bc33fbecc908ee11134d19e043e2
suy <sugarry2846@gmail.com>
2025-11-14 17:22:35 +0900

    feat: add def

--- proj.py
+++ proj.py
@@ -1,1 +1,37 @@
-print('a')
+from random import randint, sample
+
+def allChainedPathsAreShort(n):
+
+def randomTrialResult(coin):
+
+# TEST ----------------------------------------------------
+# main :: IO ()
+def main():
+
+# ------------------------DISPLAY--------------------------
+
+# fTable :: String -> (a -> String) ->
+# (b -> String) -> (a -> b) -> [a] -> String
+def fTable(s):
+
+def fName(f):
+
+
+# ------------------------GENERIC -------------------------
+
+# Just :: a -> Maybe a
+def Just(x):
+
+def Nothing():
+
+def difference(xs):
+
+def randomRInt(m):
+
+def unfoldr(f):
+
+
+# MAIN ---
+if __name__ == '__main__':
+    main()
+
